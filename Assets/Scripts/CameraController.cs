8e5229d476b4002212b8db93680da57727a023aa
Lisa Chen <58052077+l13a@users.noreply.github.com>
2020-10-19 03:17:29 -0700

    Finished Step 2 + 3

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player; //reference for player
+    private Vector3 offset; //store the offset
+    // Start is called before the first frame update
+    void Start()
+    {
+        //offset needs to be calculated once at the start
+        offset = transform.position - player.transform.position; //camera's position - players
+    }
+
+    // Update is called once per frame
+    void LateUpdate()//will call after all the updates are done
+    {
+        transform.position = player.transform.position + offset;//only match position, not rotation
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine; 
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 0; //if public, can find in inspector
+	private Rigidbody rb;
+    private float movementX;
+    private float movementY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    	rb = GetComponent<Rigidbody>(); //get the rigitbody compoenent from the game object player(which exists since you "added compoenent" earlier)
+        
+    }
+
+    void OnMove(InputValue movementValue)
+    {
+    	Vector2 movementVector = movementValue.Get<Vector2>(); //get a 2 dim vector from input
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY); //the f after 0.0 shows that this is float
+    	rb.AddForce(movement * speed);
+    }
+
+}
